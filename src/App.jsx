8c45ab9ba66a94405d0689a105ff3d965f620b87
jaseemuddin <njaseemuddin@gmail.com>
2024-07-28 23:29:10 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import { Route, Router, Routes } from "react-router-dom";
 import "./App.css";
 import { lazy, Suspense } from "react";
-import Loader from "./Components/Loader/Loader";
+// import Loader from "./Components/Loader/Loader";
+import Shop from "./Components/Shop/Shop";
+import Home from "./Components/Home/Home";
 
-const Shop = lazy(()=> import('./Components/Shop/Shop'));
-const Home = lazy(()=> import('./Components/Home/Home'));
+// const Shop = lazy(()=> import('./Components/Shop/Shop'));
+// const Home = lazy(()=> import('./Components/Home/Home'));
 
 function App() {
   return (
     <>
-      <Suspense fallback={<Loader />}>
+      {/* <Suspense fallback={<Loader />}> */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
       </Routes>
-      </Suspense>
+      {/* </Suspense> */}
     </>
   );
 }
